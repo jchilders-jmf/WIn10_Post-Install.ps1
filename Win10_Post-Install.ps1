64a49c8be9576e2351ee7dc7fbda3ffef1acb326
Chase J <9084598+TheChaseJ@users.noreply.github.com>
2019-05-14 15:42:06 -0500

    Update Win10_Post-Install.ps1

--- Win10_Post-Install.ps1
+++ Win10_Post-Install.ps1
@@ -360,15 +360,19 @@ Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer 
 
 }
 
-Function debloater {
-#Unpins all tiles from the Start Menu
-    Write-Output "Unpinning all tiles from the start menu"
+Function UnpinStart {
+    #https://superuser.com/questions/1068382/how-to-remove-all-the-tiles-in-the-windows-10-start-menu
+    #Unpins all tiles from the Start Menu
+    Write-Host "Unpinning all tiles from the start menu"
     (New-Object -Com Shell.Application).
     NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').
     Items() |
-    %{ $_.Verbs() } |
-    ?{$_.Name -match 'Un.*pin from Start'} |
-    %{$_.DoIt()}
+        % { $_.Verbs() } |
+        ? {$_.Name -match 'Un.*pin from Start'} |
+        % {$_.DoIt()}
+}
+
+Function debloater {
 
 $Keys = @(
             
@@ -534,3 +538,4 @@ removethese
 protectprivacy
 betterpreferences
 debloater
+UnpinStart
