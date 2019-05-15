e869c617987e66ec0a950203ed3c741e8c9e9d9e
Chase J <9084598+TheChaseJ@users.noreply.github.com>
2019-05-15 10:47:57 -0500

    Update Win10_Post-Install.ps1
    Added WinRM settings to Line 496
    Added Remote Registry settings to Line 500

--- Win10_Post-Install.ps1
+++ Win10_Post-Install.ps1
@@ -493,6 +493,18 @@ if ($version -eq 10) {
 	Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value "0"
 }
 
+#Enable and start Windows Remote Management (WS-Management)
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\WinRM" -Name Start -Type Dword -Value 2
+Start-Service WinRM
+
+# Enable and start Remote Registry
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\RemoteRegistry" -Name ImagePath -Type String -Value "%SystemRoot%\system32\svchost.exe -k localService -p"
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\RemoteRegistry" -Name DisplayName -Type String -Value "Remote Registry"
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\RemoteRegistry" -Name ObjectName -Type String -Value "NT AUTHORITY\LocalService"
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\RemoteRegistry" -Name Start -Type Dword -Value 2
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\RemoteRegistry" -Name Type -Type Dword -Value 32
+Start-Service RemoteRegistry
+
 #Enable RDP and allow it through the firewall
 Set-RemoteDesktopConfig -Enable -ConfigureFirewall -AllowOlderClients
    
