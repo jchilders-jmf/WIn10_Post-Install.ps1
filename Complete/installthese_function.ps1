283294bf1926b8a61e7360252eb34db31ea20968
Chase J <9084598+TheChaseJ@users.noreply.github.com>
2019-05-17 11:59:51 -0500

    Update installthese_function.ps1

--- Complete/installthese_function.ps1
+++ Complete/installthese_function.ps1
@@ -1,5 +1,6 @@
 # Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco feature enable -n allowGlobalConfirmation
 
 Function installthese {
     #--- Install Apps ---
